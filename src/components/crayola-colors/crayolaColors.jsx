679d8f5757bca89d8e58d792dcdb2b4f7a8f4a77
Lucy <lucycoe07@gmail.com>
2025-08-04 02:25:28 +0100

    added crayon tip effect and shadows

--- src/components/crayola-colors/crayolaColors.jsx
+++ src/components/crayola-colors/crayolaColors.jsx
@@ -42,17 +42,17 @@ const handleColorPaint = (slot) => {
 
 
   return (
-    <div className='pb-60 bg-yellow-400'>
+    <div className='pb-60 bg-yellow-400 font-display3'>
   
       <div className="heading-wrapper relative flex flex-col items-center mt-10 flex">
       <div className="semicircle-bg absolute">
       </div>
         <img src = './crayola.png' className='z-20 w-150'/>
-      <h1 className='mt-0 font-display3 text-center text-8xl text-red-500 relative z-10 mt-2'>COLORS</h1>
+      <h1 className='mt-0 text-center text-8xl text-red-500 relative z-10 mt-2'>COLORS</h1>
 
     </div>
 
-      <div className='bg-green-400 p-4  mx-20 xl:mx-80 rounded-xl shadow mt-5'>
+      <div className='bg-green-400 p-4  mx-20 xl:mx-80 rounded-xl shadow shadow-black mt-5'>
         <ul className='colorgrid'>
           {colors.map((color) => (
             <li
@@ -67,7 +67,7 @@ const handleColorPaint = (slot) => {
       </div>
 
       <div className='relative pt-10 pb-20'>
-        <h1 className='font-display3 text-4xl absolute mr-70 xl:mr-130 text-red-500' style={{ right: '10rem', top: '4rem' }}>
+        <h1 className=' text-4xl absolute mr-70 xl:mr-130 text-red-500' style={{ right: '10rem', top: '4rem' }}>
           Color Palette:
         </h1>
         <div>
