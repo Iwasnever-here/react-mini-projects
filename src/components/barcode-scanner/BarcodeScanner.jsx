09e27ffddf3c221b4893a98baa5fa3fb0de777d4
Lucy <lucycoe07@gmail.com>
2025-08-12 17:28:55 +0100

    added conditional rendering, light styling and make it so product ingredients are displayed

--- src/components/barcode-scanner/BarcodeScanner.jsx
+++ src/components/barcode-scanner/BarcodeScanner.jsx
@@ -6,7 +6,9 @@ const BarcodeScanner = () => {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
   const [productName, setProductName] = useState("");
+  const [productIngredients, setProductIngredients] = useState("")
   const [showScanner, setShowScanner] = useState(false);
+  const [showProduct, setShowProduct] = useState(false)
 
   const fetchFoodData = async (barcode) => {
     setLoading(true);
@@ -18,7 +20,9 @@ const BarcodeScanner = () => {
 
       if (json.status === 1) {
         const productName = json.product.product_name || "unknown product";
+        const productIngredients = json.product.ingredients_text || "unknown ingredients"
         setProductName(productName);
+        setProductIngredients(productIngredients)
       } else {
         setProductName(null);
       }
@@ -42,22 +46,26 @@ const BarcodeScanner = () => {
   useEffect(() => {
     if (productName) {
       setShowScanner(false);
+      setShowProduct(true)
     }
   }, [productName]);
 
 return (
     <>
       <div>
-        {!showScanner &&
-        <div>
-        <h2>Scan a Barcode!!</h2>
-        <button onClick={() => setShowScanner(true)}>Open Scanner</button>
+        {!showScanner && !showProduct  &&
+        <div className="text-center h-screen content-center">
+        <h2 className="text-5xl ">ALLERGY SCANNER</h2>
+        <button onClick={() => setShowScanner(true)}
+          className="bg-green-500 rounded-xl p-2 my-5">
+            SCAN
+            </button>
         </div>
         }
-        {showScanner && (
-          <BarcodeScannerComponent
-            width={500}
-            height={500}
+        {showScanner &&  (
+          <div className="flex flex-col text-center items-center justify-center content-center h-screen">
+            <div className="w-150 bg-red-500 border border-solid border-red-500 border-4 rounded-xl p-1">
+          <BarcodeScannerComponent 
             onUpdate={(error, result) => {
               if (result) {
                 if (barcode !== result.text) {
@@ -70,16 +78,28 @@ return (
               }
             }}
           />
+          </div>
+          <button onClick={() => setShowScanner(false)}
+          className="bg-red-500 rounded-xl p-2 my-5">
+            EXIT
+            </button>
+          </div>
         )}
 
         {loading && <p>Loading...</p>}
         {error && <p style={{ color: "red" }}>{error}</p>}
 
-        {!loading && !error && productName && !showScanner && 
-        <div>
-        <p>Last scanned item :</p>
+        {!loading && !error && productName && showProduct && 
+        <div className="text-center h-screen content-center">
+        <p>scanned item :</p>
         <p>{productName}</p>
+        <p>{productIngredients}</p>
+        <button onClick={() => setShowProduct(false)}
+          className="bg-green-500 rounded-xl p-2 my-5">
+            CLOSE
+            </button>
         </div>}
+        
       </div>
     </>
   );
