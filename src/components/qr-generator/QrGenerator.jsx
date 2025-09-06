17026d7ebbb6bda34fdae5216198dedb5848e50b
Lucy <lucycoe07@gmail.com>
2025-09-06 04:29:33 +0100

    added some styling

--- src/components/qr-generator/QrGenerator.jsx
+++ src/components/qr-generator/QrGenerator.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import QRCode from 'react-qr-code';
+import './QrGenerator.css'
 
 
 
@@ -24,28 +25,30 @@ const QrGenerator = () => {
   return (
     <div className='h-screen content-center  text-center p-10'>
         <div className="max-w-300 bg-blue-500 mx-auto grid grid-cols-1 md:grid-cols-5 h-150 rounded-xl shadow-md shadow-neutral-600 rounded-xl">
-        <div className="bg-neutral-300 border-2 border-neutral-600 md:col-span-3 md:rounded-l-xl">
+        <div className="bg-neutral-300 border-2 border-neutral-600 md:col-span-3 md:rounded-l-xl grey-section content-center">
+            <h1 className='mb-5 text-xl text-green-600 mt-5'>SETTINGS</h1>
             <p>link</p>
             <input type = 'text'
                    onChange={(e) => setTempValue(e.target.value)}
-                   placeholder='QR code value'
+                   placeholder='enter link'
             />
             <p>background color</p>
               <input type = 'text'
                    onChange={(e) => setTempBack(e.target.value)}
-                   placeholder='QR code background color'
+                   placeholder='enter hex e.g #FFFFFF'
             />
+            
             <p>color</p>
               <input type = 'text'
                    onChange={(e) => setTempFront(e.target.value)}
-                   placeholder='QR code color'
+                   placeholder='enter hex e.g #FFFFFF'
             />
           <br />
             <button onClick={handleGenerate}
-            className='p-3 bg-green-300 rounded-full'>generate</button>
+            className='p-3 bg-green-300 rounded-full mb-5'>generate</button>
         </div>
         <div className="bg-white border-2 border-neutral-600 md:col-span-2 content-center md:rounded-r-xl ">
-            <p className='mx-20 md:mx-10 aspect-square bg-red-500 my-10 '>
+            <p className='mx-20 md:mx-10 aspect-square bg-neutral-300 my-10 '>
                  {value && (
                     <QRCode
                         title={value}
@@ -57,7 +60,8 @@ const QrGenerator = () => {
                     />
                 )}
             </p>
-            <p>download</p>
+            <button
+            className='p-3 bg-green-300 rounded-full mb-5'>download</button>
         </div>
     </div>
 
